056760f6a65631dc9b031a9be0d8499ec108e265
Sasank Tadepalli <saisasank25@gmail.com>
2022-09-28 16:21:40 -0700

    Started Chat Functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,14 @@
-const { login, register } = require("../controllers/userController");
+const {
+  login,
+  register,
+  setAvatar,
+  getAllUsers,
+} = require("../controllers/userController");
 const router = require("express").Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/setAvatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers);
 
 module.exports = router;
