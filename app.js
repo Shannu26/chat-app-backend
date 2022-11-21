b927470bf3ac925b0a92b6b9c8ed55b209b19c9e
Sasank Tadepalli <saisasank25@gmail.com>
2022-11-20 19:11:48 -0800

    Started implementing Message Functionality

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messages");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL, {
